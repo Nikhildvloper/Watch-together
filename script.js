0e94db0c98039eaf34268d5a5d4d1d27387d6dc2
Nikhildvloper <123978399+Nikhildvloper@users.noreply.github.com>
2025-01-16 10:59:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.getElementById('actionButton').addEventListener('click', function() {
-    alert('Button clicked!');
+document.getElementById('fileInput').addEventListener('change', function(event) {
+  const file = event.target.files[0];
+  
+  if (file) {
+    // Save the file path to localStorage
+    const filePath = file.name;
+    localStorage.setItem('selectedFilePath', filePath);
+  }
+});
+
+document.getElementById('openVideoBtn').addEventListener('click', function() {
+  // Redirect to videoview.html
+  window.location.href = 'videoview.html';
 });
